4ce7d211e633e0565211d48ff505420417eb9743
rubenscpneto <rubenscpneto@hotmail.com>
2026-07-14 09:29:26 -0300

    nova foto - samurai

--- src/components/BirthdayDashboard.jsx
+++ src/components/BirthdayDashboard.jsx
@@ -39,7 +39,7 @@ const generateMockData = () => {
     { id: 16, name: 'Alexandre', surname: '', photo: 'images/alexandre.png', birthday: '05-13', department: 'Analista' },
     { id: 17, name: 'Lucas', surname: '', photo: '', birthday: '03-14', department: 'Infra' },
     { id: 18, name: 'Rian', surname: '', photo: '', birthday: '01-06', department: 'Infra' },
-    { id: 19, name: 'Samurai', surname: '', photo: '', birthday: '07-14', department: 'Infra' },
+    { id: 19, name: 'Samurai', surname: '', photo: 'images/samurai.jpeg', birthday: '07-14', department: 'Infra' },
     { id: 20, name: 'Sidnei', surname: '', photo: '', birthday: '02-13', department: 'Diretor de Infra' },
     { id: 21, name: 'Maria Vitória', surname: '', photo: '', birthday: '09-06', department: 'Software' },
     { id: 22, name: 'Paulo', surname: '', photo: '', birthday: '06-06', department: 'Software' },
